a390000d39924c8c4ed202c436402e718c26f2c3
raulDeDevWorld <proyection009@gmail.com>
2023-07-30 12:25:51 -0400

    DC

--- src/app/(with-layout)/Administrador/Plantilla/Agregar/page.jsx
+++ src/app/(with-layout)/Administrador/Plantilla/Agregar/page.jsx
@@ -27,7 +27,7 @@ function Home() {
     const [urlPostImage, setUrlPostImage] = useState(null)
 
     const [check, setCheck] = useState(false)
-    const [categorias, setCategorias] = useState({})
+    const [categorias, setCategorias] = useState({ Otros: true })
     const inputRefCard = useMask({ mask: '____ ____ ____ ____', replacement: { _: /\d/ } });
     const inputRefDate = useMask({ mask: '__/__', replacement: { _: /\d/ } });
     const inputRefCVC = useMask({ mask: '___', replacement: { _: /\d/ } });
@@ -83,17 +83,20 @@ function Home() {
     }
 
 
-    function handlerReset () {
+    function handlerReset() {
 
-    inputRef1.current.value = ''
-    inputRef2.current.value = ''
-    inputRef3.current.value = ''
-    inputRef4.current.value = ''
-    inputRef5.current.value = ''
-    inputRef6.current.value = ''
-    inputRef7.current.value = ''
+        inputRef1.current.value = ''
+        inputRef2.current.value = ''
+        inputRef3.current.value = ''
+        inputRef4.current.value = ''
+        inputRef5.current.value = ''
+        inputRef6.current.value = ''
+        inputRef7.current.value = ''
 
-   
+
+        setState({ sistema: '1.5', disponibilidad: 'Disponible' })
+        setCategorias({ Otros: true })
+        setUserSuccess('')
     }
     function save(e) {
         e.preventDefault()
@@ -101,10 +104,10 @@ function Home() {
 
         Object.entries(categorias).map(async (i) => {
             if (i[1] == true) {
-                handlerReset()
-              await  writeUserData('Producto', { ...state, categoria: i[0], uuid: uid, distribuidor: 'Precio-Justo-SRL-Data' }, 'Precio-Justo-SRL-Data', userDB, setUserData, setUserSuccess, 'Se ha guardado correctamente', 'Perfil')
-              await  uploadStorage('Producto', postImage, uid, updateUserData)
-            // return setState({ sistema: '1.5', disponibilidad: 'Disponible' })
+                await writeUserData('Producto', { ...state, categoria: i[0], uuid: uid, distribuidor: 'Precio-Justo-SRL-Data' }, 'Precio-Justo-SRL-Data', userDB, setUserData, setUserSuccess, 'Se ha guardado correctamente', 'Perfil')
+                await uploadStorage('Producto', postImage, uid, updateUserData)
+                // return setState({ sistema: '1.5', disponibilidad: 'Disponible' })
+                return handlerReset()
             }
         })
 
@@ -125,28 +128,28 @@ function Home() {
                 <input className="hidden" onChange={manageInputIMG} accept=".jpg, .jpeg, .png, .mp4, webm" id='file' type="file" />
             </div> */}
             {success == 'Se ha guardado correctamente' && <Success>Guardado correctamente</Success>}
-            <div className="w-full flex justify-center">
-                <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10 md:w-[250px] md:h-[200px]" style={{ backgroundImage: `url(${urlPostImage})`, backgroundSize: 'contain', backgroundRepeat: 'no-repeat', backgroundPosition: 'center' }}>
+            <div className="w-full flex justify-center ">
+                <label htmlFor="fileUpload" className="mt-2 flex justify-center items-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10 md:w-[250px] md:h-[200px]" style={{ backgroundImage: `url(${urlPostImage})`, backgroundSize: 'contain', backgroundRepeat: 'no-repeat', backgroundPosition: 'center' }}>
                     <div className="text-center">
                         <svg className="mx-auto h-12 w-12 text-gray-300" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
                             <path fillRule="evenodd" d="M1.5 6a2.25 2.25 0 012.25-2.25h16.5A2.25 2.25 0 0122.5 6v12a2.25 2.25 0 01-2.25 2.25H3.75A2.25 2.25 0 011.5 18V6zM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0021 18v-1.94l-2.69-2.689a1.5 1.5 0 00-2.12 0l-.88.879.97.97a.75.75 0 11-1.06 1.06l-5.16-5.159a1.5 1.5 0 00-2.12 0L3 16.061zm10.125-7.81a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0z" clipRule="evenodd" />
                         </svg>
                         <div className="mt-4 flex text-sm leading-6 text-gray-600">
                             <label htmlFor="fileUpload" className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500">
-                                <span>Upload a file</span>
-                                <input id="fileUpload" name="frontPage" onChange={manageInputIMG} type="file" className="sr-only" accept="img/*" required />
+                                <span>Cargar Imagen</span>
+                                <input id="fileUpload" name="frontPage" onChange={manageInputIMG} type="file" className="sr-only" accept="image/*" required />
                             </label>
-                            <p className="pl-1">or drag and drop</p>
+                            <p className="pl-1">{' '} puede ser GIF</p>
                         </div>
-                        <p className="text-xs leading-5 text-gray-600">PNG, JPG, GIF up to 10MB</p>
+                        <p className="text-xs leading-5 text-gray-600">PNG, JPG, GIF max 10MB</p>
                     </div>
-                </div>
+                </label>
             </div>
             <br />
             <div className="grid gap-6 mb-6 md:grid-cols-2">
                 <div>
                     <Label htmlFor="">Nombre de Producto 1</Label>
-                    <Input type="text" name="nombre de producto 1" reference={inputRef1}  onChange={onChangeHandler} />
+                    <Input type="text" name="nombre de producto 1" reference={inputRef1} onChange={onChangeHandler} require />
                 </div>
                 <div>
                     <Label htmlFor="">Nombre de Producto 2</Label>
@@ -158,15 +161,15 @@ function Home() {
                 </div>
                 <div>
                     <Label htmlFor="">Descripción básica</Label>
-                    <Input type="text" name="descripcion basica" reference={inputRef4}   onChange={onChangeHandler} />
+                    <Input type="text" name="descripcion basica" reference={inputRef4} onChange={onChangeHandler} require />
                 </div>
                 <div>
                     <Label htmlFor="">Descripción técnica</Label>
-                    <Input type="text" name="descripcion tecnica" reference={inputRef5} onChange={onChangeHandler} />
+                    <Input type="text" name="descripcion tecnica" reference={inputRef5} onChange={onChangeHandler} require />
                 </div>
                 <div>
                     <Label htmlFor="">Usu frecuente</Label>
-                    <Input type="text" name="uso frecuente" reference={inputRef6} onChange={onChangeHandler} />
+                    <Input type="text" name="uso frecuente" reference={inputRef6} onChange={onChangeHandler} require />
                 </div>
                 <div>
                     <Label htmlFor="">Categorias</Label>
