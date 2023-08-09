519d6cb7450fd5179cb485c1a1fb5eb4cba3e7f8
raulDeDevWorld <proyection009@gmail.com>
2023-08-09 14:11:49 -0400

    DC

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -41,7 +41,7 @@ export default function Card({ nombre1, nombre2, nombre3, costo, url, empresa, d
     }
     console.log(item)
     return (
-        <div class="relative w-full bg-gray-100 min-h-[180px] max-w-[500px] rounded-[15px] border border-gray-200 rounded-[20px] shadow mt-5" onClick={(e) => seeMore(e, i)} style={{ display: 'grid', gridTemplateColumns: 'auto 150px' }}>
+        <div class="relative w-full bg-gray-100 min-h-[180px] max-w-[500px] rounded-[15px] border border-gray-200 rounded-[20px] shadow mt-5" onClick={(e) => seeMore(e, i)} style={{ display: 'grid', gridTemplateColumns: 'auto 150px', gridAutoFlow: 'dense'}}>
             <div class=" font-bold text-[16px] mb-2 text-gray-950 col-span-2 p-5 pb-0">
                 {i['nombre de producto 1']}
             </div>
@@ -59,12 +59,13 @@ export default function Card({ nombre1, nombre2, nombre3, costo, url, empresa, d
                     <p class="text-gray-700 text-[14px]">{i['descripcion basica']}</p>
                 </div>
             </div>
-            
+
             <div>
                 <div class="relative h-[150px] w-[150px] rounded-t text-center" style={{ backgroundImage: `url(${i.url})`, backgroundSize: 'contain', backgroundRepeat: 'no-repeat', backgroundPosition: 'center' }}>
                     {recetado === true && <span className='absolute text-[16px] font-bold right-10 top-10 text-green-600 transform rotate-[-45deg]'>Recetado <br /> por tu doctor</span>}
                 </div>
                 <div className='flex py-4 pr-4'>
+                    Entrega <br />
                     <span className={`block text-center w-full text-14 p-2 rounded-full text-[16px] ${i.disponibilidad == 'No disponible' && 'bg-red-400'} ${i.disponibilidad == 'Inmediatamente' && 'bg-green-400'} ${i.disponibilidad == 'En un día' && 'bg-yellow-300'}`}>{i.disponibilidad}</span>
                 </div>
             </div>
