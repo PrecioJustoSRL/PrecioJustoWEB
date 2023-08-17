c384175e4fe7c1f9658e520446341f691223b63d
raulDeDevWorld <proyection009@gmail.com>
2023-08-17 17:51:20 -0400

    DC

--- src/app/(with-layout)/Cliente/page.jsx
+++ src/app/(with-layout)/Cliente/page.jsx
@@ -200,16 +200,16 @@ function Home() {
                    
                 {filterQR.length > 0 && recetaDBP !== null && recetaDBP !== undefined &&
                         recetaDBP.sort(sortArray).map((i, index) =>
-                            tienda === 'Recetar'
-                                ? i.qr.includes(filterQR) && <CardM i={i} key={index} />
-                                : i.qr.includes(filterQR) && <Card i={i} recetado={true} key={index} />
+                            tienda === 'Recetar' 
+                                ? i.qr.includes(filterQR) && i.disponibilidad !== 'No disponible' && <CardM i={i} key={index} />
+                                : i.qr.includes(filterQR) && i.disponibilidad !== 'No disponible' && <Card i={i} recetado={true} key={index} />
                         )}
                     {filter.length == 0 && filterQR.length == 0 &&
                         productDB !== null && productDB !== undefined &&
                         productDB.sort(sortArray).map((i, index) => {
                             if (i.distribuidor !== 'Precio-Justo-SRL-Data') return tienda === 'Recetar'
-                                ? <CardM i={i} key={index} />
-                                : <Card i={i} key={index} />
+                                ? i.disponibilidad !== 'No disponible' && <CardM i={i} key={index} />
+                                : i.disponibilidad !== 'No disponible' &&  <Card i={i} key={index} />
                         }
                         )}
                     {filter.length > 0 && productDB !== null && productDB !== undefined &&
@@ -217,11 +217,11 @@ function Home() {
                             if (i.distribuidor !== 'Precio-Justo-SRL-Data') return tienda === 'Recetar' && i.distribuidor !== 'Precio-Justo-SRL-Data'
                                 ? (i['nombre de producto 1'].toLowerCase().includes(filter.toLowerCase()) ||
                                     (i['nombre de producto 2'] && i['nombre de producto 2'].toLowerCase().includes(filter.toLowerCase())) ||
-                                    (i['nombre de producto 3'] && i['nombre de producto 3'].toLowerCase().includes(filter.toLowerCase()))) &&
+                                    (i['nombre de producto 3'] && i['nombre de producto 3'].toLowerCase().includes(filter.toLowerCase())))  && i.disponibilidad !== 'No disponible' && 
                                 <CardM i={i} key={index} />
                                 : (i['nombre de producto 1'].toLowerCase().includes(filter.toLowerCase()) ||
                                     (i['nombre de producto 2'] && i['nombre de producto 2'].toLowerCase().includes(filter.toLowerCase())) ||
-                                    (i['nombre de producto 3'] && i['nombre de producto 3'].toLowerCase().includes(filter.toLowerCase()))) &&
+                                    (i['nombre de producto 3'] && i['nombre de producto 3'].toLowerCase().includes(filter.toLowerCase()))) &&  i.disponibilidad !== 'No disponible' && 
                                 <Card i={i} key={index} />
                         }
                         )}
