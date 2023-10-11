441a20cd0c69f8bd242743998c34e7516e0afdef
raulDeDevWorld <proyection009@gmail.com>
2023-10-11 13:34:46 -0400

    DC

---
+++ APK/app/src/main/java/pro/preciojusto/tienda/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package pro.preciojusto.tienda.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
